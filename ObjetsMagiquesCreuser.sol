5cf4a6873faf2d406189a22c5f234ceac9b8a141
MaxChagon <59312962+MaxChagon@users.noreply.github.com>
2020-03-29 17:15:15 +0200

    Create ObjetsMagiquesCreuser.sol

---
+++ ObjetsMagiquesCreuser.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.5.12;
+
+
+contract ObjetMagique{
+    
+    uint256 id;
+    
+    constructor(uint rarete, uint typeObjet, uint pouvoir, uint modele) public {
+        id = rarete;
+        id *= 10;
+        id += typeObjet;
+        id *= 10;
+        id += pouvoir;
+        id *= 10;
+        id += modele;
+    }
+    
+    function getId() public view returns (uint256){
+        return id;
+    }
+}
+
+contract ObjetsMagiques{
+    
+    mapping (uint256 => address) public possesseurs;
+    mapping (uint256 => bool) public existence;
+    
+    event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
+
+    function balanceOf(address _owner) public view returns (uint256 balance){
+        return _owner.balance;
+    }
+    
+    function ownerOf(uint256 _tokenId) public view returns (address _owner){
+        address retour = possesseurs[_tokenId];
+        return retour;
+    }
+    
+    function exists(uint256 _tokenId) public view returns (bool _exists){
+        return existence[_tokenId];
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) public {
+        require(_from != address(0));
+        require(_to != address(0));
+        require(possesseurs[_tokenId] == _from);
+        possesseurs[_tokenId] = _to;
+    }
+    
+    
+    function creuser() public payable returns (ObjetMagique){
+        require(msg.value == 1e17);
+        uint rarete = uint8(blockhash(block.number-1)[0])%3;
+        uint typeObjet = uint8(blockhash(block.number-1)[1])%3;
+        uint pouvoir = uint8(blockhash(block.number-1)[2])%3;
+        uint modele = uint8(blockhash(block.number-1)[3])%3;
+        ObjetMagique nouvelObjet = new ObjetMagique(rarete,typeObjet,pouvoir,modele);
+        if (existence[nouvelObjet.getId()] == true){
+            nouvelObjet = creuser();
+        }
+        else{
+            existence[nouvelObjet.getId()] = true;
+        }
+        return nouvelObjet;
+        
+    }
+    
+}
